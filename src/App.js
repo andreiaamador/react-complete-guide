b2856512ce45c3409e72e4bc86418ffb537cc696
Andreia Amador <andreiaamadort92@gmail.com>
2022-08-08 15:50:40 +0200

    working with fragments, portals and refs

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import Expense from "./components/expenses/Expense";
-import NewExpense from "./components/newExpense/NewExpense";
+import React, { useState } from 'react';
 
-import { useState } from "react";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [usersList, setUsersList] = useState([]);
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((previousExpenses) => {
-      return [expense, ...previousExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
+
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expense expenses={expenses}></Expense>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
